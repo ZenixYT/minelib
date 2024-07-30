fdc15b0775dfd08df0977136ff36776e22f12299
Anthony <carvekidsak@gmail.com>
2024-07-29 21:24:41 -0800

    working multifile

--- minelib/__init__.py
+++ minelib/__init__.py
@@ -1,8 +1,8 @@
 from minelib.Pack import Pack
-from minelib.types.PlayerSpecifier import PlayerSpecifier
-from minelib.types.Location import Location
-from minelib.types.ItemStack import ItemStack, ItemMeta
-from minelib.types.ItemComponent import ItemComponent
-from minelib.types.CraftingRecipe import CraftingRecipe, CraftingKey
-from minelib.types.Execute import Execute, ScoreMatchesOps
-from minelib.types.TextComponent import TextComponent, TextColor, TextFormat
+from minelib.lib.types.PlayerSpecifier import PlayerSpecifier
+from minelib.lib.types.Location import Location
+from minelib.lib.types.ItemStack import ItemStack, ItemMeta
+from minelib.lib.types.ItemComponent import ItemComponent
+from minelib.lib.types.CraftingRecipe import CraftingRecipe, CraftingKey
+from minelib.lib.types.Execute import Execute, ScoreMatchesOps
+from minelib.lib.types.TextComponent import TextComponent, TextColor, TextFormat
--- minelib/types/CraftingRecipe.py
+++ minelib/types/CraftingRecipe.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from minelib.types.ItemStack import ItemStack, ItemMeta
+from minelib.lib.types.ItemStack import ItemStack, ItemMeta
 
 @dataclass
 class CraftingKey:
